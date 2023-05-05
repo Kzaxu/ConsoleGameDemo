fb444cbc253d73579a3906b97d2026cad43278cc
xuxu <Kzaxu@github.com>
2023-05-05 23:52:30 +0800

    add test game of life

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "game_of_life.hpp"
 
 
-int SDL_main(int, char*[])
-{
-	// Use olcConsoleGameEngine derived app
+void TestGameOfLife() {
 	GameOfLife game(GameOfLife::InitMethod::GosperGliderGun);
 	game.ConstructConsole(200, 150, 4, 4);
 	game.Start();
+}
+
+
+int SDL_main(int, char*[])
+{
+	// Use olcConsoleGameEngine derived app
+	TestGameOfLife();
 	return 0;
 }
